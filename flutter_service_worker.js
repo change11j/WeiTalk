382c285ba3a8a9b890e7c9cfbe85918592e64fdd
change11j <change11j@users.noreply.github.com>
2024-07-19 16:13:54 +0000

    deploy: 92f2bac1915311a5a303404729326047c9febd75

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "fe85db0d9e6f71c782457e6ab6d77fb8",
+"flutter_bootstrap.js": "fa2a66625a7c2e34992bae95b6c2f0d3",
 "version.json": "b9ebcf5e41e76f91c15b5fc810ee6c21",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
